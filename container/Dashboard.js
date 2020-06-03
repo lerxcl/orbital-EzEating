f6846c6a9c6b5c370b311d289258b5480bfbea32
Jerlyn <jjerlynlim@gmail.com>
2020-06-03 19:38:28 +0800

    create rough dashboard

--- container/Dashboard.js
+++ container/Dashboard.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import { Text } from 'react-native';
+import {Alert, Image, StyleSheet, TextInput, Text, View, KeyboardAvoidingView, TouchableOpacity} from "react-native";
 
 class Dashboard extends React.Component {
     render() {
         return (
-            <Text>Main Screen!</Text>
-            )
+            <View style={styles.container}>
+                <Text style={styles.text}>
+                    All shops
+                </Text>
+                <Text> Favourites </Text>
+                <Text style={styles.text}>
+                    Shop ABC
+                </Text>
+                <Text style={styles.text}>
+                    Shop 123
+                </Text>
+      </View>
+      )
     }
 }
 
 export default Dashboard;
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    },
+    text: {
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 25,
+        paddingHorizontal: 16,
+        width: 300,
+        fontSize: 16,
+        marginVertical: 10,
+        paddingVertical: 10
+    },
+});
