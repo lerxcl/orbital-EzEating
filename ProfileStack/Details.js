6650d6e453106c813ce9833cef61ca6db681bdb0
Jerlyn <jjerlynlim@gmail.com>
2020-06-12 15:37:10 +0800

    added new folder for profile stack

---
+++ ProfileStack/Details.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+class Details extends React.Component {
+    render() {
+        return (
+            <Text>Details</Text>
+            )
+    }
+}
+
+export default Details;
