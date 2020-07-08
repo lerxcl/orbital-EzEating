559d311e4e94b6bd9b00a816c3765ba5a4c4630c
Jerlyn <jjerlynlim@gmail.com>
2020-07-08 20:53:34 +0800

    added maps

--- ProfileStack/UserHistory.js
+++ ProfileStack/UserHistory.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Text } from 'react-native';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 
 class UserHistory extends React.Component {
+    
     render() {
         return (
-            <Text>History</Text>
-            )
+            <MapView
+                style={{ flex: 1 }}
+                provider = {PROVIDER_GOOGLE}
+                initialRegion={{
+                    latitude: 1.290270,
+                    longitude: 103.851959,
+                    latitudeDelta: 0.05,
+                    longitudeDelta: 0.05
+                }}
+            >
+            </MapView>
+        )
     }
 }
 
